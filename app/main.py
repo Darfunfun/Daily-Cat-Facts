0f9ec610fc5023846c9dba55a0b9b31f7c1d2588
Yamine <ybaghdadi.pro@protonmail.com>
2025-12-15 00:05:41 +0100

    Ajout de la dimension web du projet

--- app/main.py
+++ app/main.py
@@ -1,18 +1,58 @@
+###############################################
+# Flask : Micro serveur web
+# render_template_string : Afficher du HTML depuis Python
+# jsonify : Traduction de dictionnaire Python en JSON pour nos navigateurs
+# requests : Pour interagir avec les APIs
+###############################################
+
 import requests
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html") # retourne le body HTTP de type HTML
+
+@app.route("/catfacts")
+def catfactHome():
+    return render_template("catfacts.html") # retourne le body  HTTP de type HTML
+
+@app.route("/catfacts/fact")
+def getCatFact():
+    myCatRequest = requests.get("https://catfact.ninja/fact", timeout=5) # Affiche un json, avec fact: "str" et length: int
+    myCatRequest = myCatRequest.json()
+    #print(myCatRequest)
+    return jsonify(myCatRequest) # retourne le body HTTP de type json
 
 
-try:
-    r = requests.get('https://catfact.ninja/fact', timeout=5)
-    r.raise_for_status()
-    print("L'API repond !")
+app.run(debug=True)
 
-    data = r.json()
-    print("Voici un fait sur les chats : ", data["fact"])
 
-except requests.exceptions.RequestException as e:
-    print("Probleme d'API... Code erreur : ", e)
-    exit (1)
-except ValueError as e:
-    print("Probleme de décodage JSON... Code erreur : ", e)
-    exit (1)
+
+
+
+
+
+# OLD : 
+
+# import requests
+
+
+# try:
+#     r = requests.get('https://catfact.ninja/fact', timeout=5)
+#     r.raise_for_status()
+#     print("L'API repond !")
+
+#     data = r.json()
+#     print("Voici un fait sur les chats : ", data["fact"])
+
+# except requests.exceptions.RequestException as e:
+#     print("Probleme d'API... Code erreur : ", e)
+#     exit (1)
+# except ValueError as e:
+#     print("Probleme de décodage JSON... Code erreur : ", e)
+#     exit (1)
+
+
 
