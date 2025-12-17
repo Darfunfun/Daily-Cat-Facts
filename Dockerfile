e80a228a85a578e5ba96b46c9887483a4d9e8db0
Yamine <ybaghdadi.pro@protonmail.com>
2025-12-17 15:16:21 +0100

    CatFact - Correction avec les B1

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM python:3.13.7-alpine
 
-RUN adduser --disabled-password --gecos "" dailycatfacts_user
-USER dailycatfacts_user
+# RUN adduser --disabled-password --gecos "" dailycatfacts_user
+# USER dailycatfacts_user
 
 WORKDIR /app
 
@@ -12,3 +12,51 @@ RUN pip install --no-cache-dir -r requirements.txt
 COPY . .
 
 CMD ["python3", "app/main.py"]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# TP Guardia : 
+# # Choisir une image Python
+# FROM python:3.11-slim
+
+# # Définir le dossier de travail dans le container
+# WORKDIR /app
+
+# # Copier les fichiers requirements
+# COPY requirements.txt .
+
+# # Installer les dépendances
+# RUN pip install --no-cache-dir -r requirements.txt
+
+# # Copier le reste du code
+# COPY . .
+
+# # Exposer le port de Flask
+# EXPOSE 5000
+
+# # Commande pour lancer l'application
+# CMD ["python", "app.py"]
+
+
+# Build / Run : 
+# docker build -t tamagotchi-api .
+# docker run -p 5000:5000 tamagotchi-api
+
+# Volume : 
+# docker run -p 5000:5000 -v $(pwd)/data:/app/data tamagotchi-api
+
--- app/main.py
+++ app/main.py
@@ -6,54 +6,106 @@
 ###############################################
 
 import requests
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 
-app = Flask(__name__)
 
-@app.route("/")
-def home():
+siteweb = Flask(__name__)
+
+
+
+@siteweb.route("/")
+def Home():
     return render_template("index.html") # retourne le body HTTP de type HTML
 
-@app.route("/catfacts")
-def catfactHome():
-    return render_template("catfacts.html") # retourne le body  HTTP de type HTML
+#----------------------------------------------------------------------------------------------------------------
 
-@app.route("/catfacts/fact")
-def getCatFact():
-    myCatRequest = requests.get("https://catfact.ninja/fact", timeout=5) # Affiche un json, avec fact: "str" et length: int
-    myCatRequest = myCatRequest.json() # .json() ouvre l’enveloppe de Response et transforme le body JSON (texte) en dict Python
-    #print(myCatRequest)
-    return jsonify(myCatRequest) # transforme le dict Python en JSON texte, crée une réponse HTTP, retourne le body HTTP de type json
+@siteweb.route("/catfacts_guardia")
+def Catfact_home():
+    return render_template("catfacts.html")
 
 
-
-app.run(debug=True)
+@siteweb.route("/catfacts_guardia/fact")
+def Show_catfact():
+    maRequestCatfact = requests.get("https://catfact.ninja/fact")
+    maRequestCatfact = maRequestCatfact.json()
+    return maRequestCatfact
 
 
 
+# @siteweb.route("/catfacts")
+# def catfactHome():
+#     return render_template("catfacts.html") # retourne le body  HTTP de type HTML
+
+# @siteweb.route("/catfacts/fact")
+# def getCatFact():
+#     myCatRequest = requests.get("https://catfact.ninja/fact", timeout=5) # Affiche un json, avec fact: "str" et length: int
+#     myCatRequest = myCatRequest.json() # .json() ouvre l’enveloppe de Response et transforme le body JSON (texte) en dict Python
+#     #print(myCatRequest)
+#     return jsonify(myCatRequest) # transforme le dict Python en JSON texte, crée une réponse HTTP, retourne le body HTTP de type json
+
+# #----------------------------------------------------------------------------------------------------------------
 
 
 
 
-# OLD : 
-
-# import requests
 
 
-# try:
-#     r = requests.get('https://catfact.ninja/fact', timeout=5)
-#     r.raise_for_status()
-#     print("L'API repond !")
 
-#     data = r.json()
-#     print("Voici un fait sur les chats : ", data["fact"])
 
-# except requests.exceptions.RequestException as e:
-#     print("Probleme d'API... Code erreur : ", e)
-#     exit (1)
-# except ValueError as e:
-#     print("Probleme de décodage JSON... Code erreur : ", e)
-#     exit (1)
+
+
+
+
+
+
+
+# @siteweb.route("/pokeapi")
+# def pokeapiHome():
+#     return render_template("pokeapi.html")
+
+# @siteweb.route("/pokeapi/getpokemon")
+# def getPokemon():
+#     pokeName = request.args.get("name")
+#     print(pokeName)
+#     myPokeRequest = requests.get("https://pokeapi.co/api/v2/pokemon/{pokeName}")
+#     print(myPokeRequest)
+#     myPokeRequest = myPokeRequest.json()
+#     return jsonify(myPokeRequest)
+
+
+
+
+
+
+# #----------------------------------------------------------------------------------------------------------------
+
+siteweb.run(debug=True)
+
+
+
+
+
+
+
+# # OLD : 
+
+# # import requests
+
+
+# # try:
+# #     r = requests.get('https://catfact.ninja/fact', timeout=5)
+# #     r.raise_for_status()
+# #     print("L'API repond !")
+
+# #     data = r.json()
+# #     print("Voici un fait sur les chats : ", data["fact"])
+
+# # except requests.exceptions.RequestException as e:
+# #     print("Probleme d'API... Code erreur : ", e)
+# #     exit (1)
+# # except ValueError as e:
+# #     print("Probleme de décodage JSON... Code erreur : ", e)
+# #     exit (1)
 
 
 
